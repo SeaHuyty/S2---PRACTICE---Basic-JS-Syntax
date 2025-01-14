777b3b23f9c89705754f3e09dd945aead8f5fccd
SeaHuyty <seahuyty4444@gmail.com>
2025-01-14 20:41:08 +0700

    Update challenge05.js

--- challenge05.js
+++ challenge05.js
@@ -1,11 +1,6 @@
 function challenge5(string) {
-    let identifier = 1;
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] == " ") {
-            identifier++;
-        }
-    }
-    return identifier;
+    let splitString = string.split(" ");
+    return splitString.length;
 }
 
 let text = "a bb ccc ddddddd e";
